93d9dbfca40b6309b3672143169601b045d9b2a9
Amitabh Kumar <gaurav.official133@gmail.com>
2021-06-12 17:42:01 +0530

    blog commit

--- ashish_bhardwaj/settings.py
+++ ashish_bhardwaj/settings.py
@@ -143,10 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+STATIC_URL  =  '/static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT  =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL   =  '/media/'
 
 
 
